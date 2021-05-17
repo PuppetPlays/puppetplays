177aa787c06f003c2abe7fd1c223bdfcd4bb3eac
Thomas Paillot <paillot.t@intactile.com>
2021-05-17 14:53:36 +0200

    Feature: Add a navigation menu in the header

--- puppetplays-web/components/Header.js
+++ puppetplays-web/components/Header.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import useTranslation from 'next-translate/useTranslation';
 import LanguageSelector from 'components/LanguageSelector';
 import styles from './header.module.scss';
 
 function Header({ children }) {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.container}>
       <h1 className={styles.logo}>
@@ -13,6 +16,20 @@ function Header({ children }) {
           </a>
         </Link>
       </h1>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/auteurs">
+              <a>{t('common:authors')}</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/techniques-d-animation">
+              <a>{t('common:animationTechniques')}</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
       {children}
       <div className={styles.languageSelector}>
         <LanguageSelector />
