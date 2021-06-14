478d3874e892242c83aba8d8bae714afecec26be
Thomas Paillot <paillot.t@intactile.com>
2021-06-14 17:11:49 +0200

    Enhancement: The user can now launch a search on an author or an animation technique from their menu

--- puppetplays-web/components/NoteDropdownMenu.js
+++ puppetplays-web/components/NoteDropdownMenu.js
@@ -1,5 +1,8 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
+import cond from 'lodash/cond';
+import constant from 'lodash/constant';
+import stubTrue from 'lodash/stubTrue';
 import useTranslation from 'next-translate/useTranslation';
 import DropdownMenu, { DropdownMenuItem } from 'components/DropdownMenu';
 import { modalTypes, useModal } from 'components/modalContext';
@@ -9,16 +12,46 @@ function NoteDropdownMenu({ id, modalType }) {
 
   const [, dispatch] = useModal();
 
-  const handleClick = useCallback(() => {
+  const handleOpenNote = useCallback(() => {
     dispatch({
       type: 'open',
       payload: { type: modalType, meta: { id } },
     });
   }, [dispatch, id, modalType]);
 
+  const handleSearchWith = useCallback(() => {
+    const isAuthorMenu = () => modalType === modalTypes.author;
+    const isAnimationTechniqueMenu = () =>
+      modalType === modalTypes.animationTechnique;
+    const url = cond([
+      [isAuthorMenu, constant(`/repertoire?authors=${id}`)],
+      [
+        isAnimationTechniqueMenu,
+        constant(`/repertoire?animationTechniques=${id}`),
+      ],
+      [stubTrue, constant(null)],
+    ])();
+
+    if (url) {
+      const { protocol, host } = window.location;
+      window.location = `${protocol}//${host}${url}`;
+    }
+  }, [id, modalType]);
+
   return (
-    <DropdownMenu itemsCount={1}>
+    <DropdownMenu itemsCount={2}>
       {[
+        <DropdownMenuItem
+          key="search"
+          icon={
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+              <path d="M11 10.4c1-1.1 1.6-2.6 1.6-4.1a6.3 6.3 0 10-2.8 5.2l4.4 4.4 1.2-1.1-4.4-4.4zM11 6.3a4.6 4.6 0 11-9.2 0 4.6 4.6 0 019.2 0z" />
+            </svg>
+          }
+          onClick={handleSearchWith}
+        >
+          {t('common:launchSearch')}
+        </DropdownMenuItem>,
         <DropdownMenuItem
           key="note"
           icon={
@@ -26,7 +59,7 @@ function NoteDropdownMenu({ id, modalType }) {
               <path d="M5.3 7.1v-.6l.8-.2A12.4 12.4 0 019.2 6l.4.2-1.8 7.4h1.4v.6a5.7 5.7 0 01-2.1.7h-.8c-.5 0-.8 0-1-.2-.2-.1-.3-.3-.3-.5v-.7l.2-.7 1.4-5.4-1.3-.2zM7 3.2c0-.4.1-.7.4-.9.3-.2.7-.3 1.2-.3s.9.1 1.1.3c.3.2.5.5.5 1 0 .3-.2.6-.5.8a2 2 0 01-1.1.3c-.5 0-.9 0-1.2-.3a1 1 0 01-.4-.9z" />
             </svg>
           }
-          onClick={handleClick}
+          onClick={handleOpenNote}
         >
           {t('common:openNote')}
         </DropdownMenuItem>,
