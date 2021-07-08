363b991741dd4d76eae79cc3054e0fda0fe68c54
Thomas Paillot <paillot.t@intactile.com>
2021-07-08 12:03:40 +0200

    Chore: Update react and nextjs and add OpenLayers module

--- puppetplays-web/next.config.js
+++ puppetplays-web/next.config.js
@@ -1,9 +1,10 @@
 const { locales, defaultLocale } = require('./i18n.json');
+const withTranspile = require('next-transpile-modules')(['ol']);
 
-module.exports = {
+module.exports = withTranspile({
   poweredByHeader: false,
   i18n: {
     locales,
     defaultLocale,
   },
-};
+});
