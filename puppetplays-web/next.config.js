ab626a83a9a7032d8997825aea6d8c4f56c39600
Thomas Paillot <paillot.t@intactile.com>
2021-07-09 12:57:22 +0200

    Build: Ignore eslint errors when building

--- puppetplays-web/next.config.js
+++ puppetplays-web/next.config.js
@@ -7,4 +7,9 @@ module.exports = withTranspile({
     locales,
     defaultLocale,
   },
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 });
