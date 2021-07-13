64a7dcae9ccee5a826e58e96441bb9950322fef2
Thomas Paillot <paillot.t@intactile.com>
2021-07-13 12:59:41 +0200

    Style: Remove unused code

--- puppetplays-web/components/Header.js
+++ puppetplays-web/components/Header.js
@@ -1,13 +1,10 @@
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import useTranslation from 'next-translate/useTranslation';
 import LanguageSelector from 'components/LanguageSelector';
 import MainNav from 'components/MainNav';
 import styles from './header.module.scss';
 
 function Header({ children }) {
-  const { t } = useTranslation();
-
   return (
     <header className={styles.container}>
       <h1 className={styles.logo}>
