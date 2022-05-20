58fd4d47b9bd6f12782eab0dcb1f395fbc441e01
Thomas Paillot <paillot.t@intactile.com>
2022-05-20 13:43:42 +0200

    Fix: Migrate the drafts and the pending entries characters to the new format

---
+++ puppetplays-admin/migrations/m220520_085031_migrateWorksCharactersOfDraftsToNewFormat.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace craft\contentmigrations;
+
+use Craft;
+use craft\db\Migration;
+use craft\elements\Entry;
+
+/**
+ * m220520_085031_migrateWorksCharactersOfDraftsToNewFormat migration.
+ */
+class m220520_085031_migrateWorksCharactersOfDraftsToNewFormat extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $notLiveEntries = Entry::find()
+            ->status(['disabled', 'pending', 'expired'])
+            ->section('works')
+            ->with(['characters'])
+            ->all();
+        $draftEntries = Entry::find()
+            ->drafts()
+            ->status(null)
+            ->section('works')
+            ->with(['characters'])
+            ->all();
+        
+        $entries = array_merge($notLiveEntries, $draftEntries);
+        
+        foreach ($entries as $entry) {
+            if (count($entry->characters) > 0) {
+                $number = 0;
+                $textCharactersData = array();
+
+                foreach ($entry->characters as $originalCharacter) {
+                $number++;
+                $textCharactersData["new$number"] = array(
+                    'type' => 'character',
+                    'fields' => array(
+                        'nameInText' => $originalCharacter->textName,
+                        'roles' => $originalCharacter->character->ids()
+                    )
+                );
+                }
+
+                $entry->setFieldValue('textCharacters', $textCharactersData);
+            }
+
+            
+            $success = Craft::$app->elements->saveElement($entry);
+
+            if (!$success) {
+                Craft::error('Couldn’t migrate the characters field to the new format', __METHOD__);
+            }
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $elementsService = Craft::$app->getElements();
+        $notLiveEntries = Entry::find()
+            ->status(['disabled', 'pending', 'expired'])
+            ->section('works')
+            ->with(['characters'])
+            ->all();
+        $draftEntries = Entry::find()
+            ->drafts()
+            ->status(null)
+            ->section('works')
+            ->with(['characters'])
+            ->all();
+
+        $entries = array_merge($notLiveEntries, $draftEntries);
+
+        foreach ($entries as $entry) {
+            if (count($entry->textCharacters) > 0) {
+                foreach ($entry->textCharacters as $textCharacter) {
+                    $elementsService->deleteElementById($textCharacter->id, null, null, false);
+                }
+            }
+        }
+    }
+}
