a86fb0e7d991fb19779ab86534b6c4434e65a3c8
Thomas Paillot <paillot.t@intactile.com>
2022-06-02 11:45:46 +0200

    Auto update the position of the dropdown

--- puppetplays-web/components/DropdownMenuAdvanced.js
+++ puppetplays-web/components/DropdownMenuAdvanced.js
@@ -14,6 +14,7 @@ import {
   offset,
   flip,
   shift,
+  autoUpdate,
   useListNavigation,
   useInteractions,
   useRole,
@@ -53,6 +54,7 @@ export const MenuComponent = forwardRef(
       onOpenChange: setOpen,
       middleware: [offset({ mainAxis: 8, alignmentAxis: 0 }), flip(), shift()],
       placement: 'bottom-start',
+      whileElementsMounted: autoUpdate,
     });
 
     const { getReferenceProps, getFloatingProps, getItemProps } =
