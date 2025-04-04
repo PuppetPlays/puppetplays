595fde3ae0d32b1740fce7b8f2db961d1e217288
ReneMarceau <rene.marceau.dev@gmail.com>
2025-04-04 11:28:01 -0400

    Update packages: yarn add @testing-library/jest-dom@latest @testing-library/react@latest

--- puppetplays-web/__mocks__/query-string.js
+++ puppetplays-web/__mocks__/query-string.js
@@ -1,18 +1,1 @@
-module.exports = {
-  stringify: jest.fn((obj, options) => {
-    return Object.entries(obj)
-      .filter(([, value]) => (options?.skipNull ? value !== null : true))
-      .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-      .join('&');
-  }),
-  parse: jest.fn((str) => {
-    if (!str) return {};
-    return str
-      .split('&')
-      .map(param => param.split('='))
-      .reduce((acc, [key, value]) => ({
-        ...acc,
-        [key]: decodeURIComponent(value),
-      }), {});
-  })
-}; 
+ 
--- puppetplays-web/jest-setup.js
+++ puppetplays-web/jest-setup.js
@@ -1,1 +1,5 @@
+// Import essential testing libraries
 import '@testing-library/jest-dom';
+
+// Set test environment
+global.process.env.NODE_ENV = 'test';
